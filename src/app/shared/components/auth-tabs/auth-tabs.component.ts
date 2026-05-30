6b00fbc227cfc4fe03738103befd39f0e8af8ca2
Nada-Elhawary <nada.elhawaryy3@gmail.com>
2026-05-30 18:39:06 +0300

    Reapply "feat(auth): remove role from loign page"
    This reverts commit 4ac229d1f2a2cc9f0093032c59e36f48132edf7c.

--- src/app/shared/components/auth-tabs/auth-tabs.component.ts
+++ src/app/shared/components/auth-tabs/auth-tabs.component.ts
@@ -11,4 +11,8 @@ import { CommonModule } from '@angular/common';
 export class AuthTabsComponent {
   @Input() activeTab: 'signin' | 'signup' = 'signin';
   @Output() onTabChange = new EventEmitter<'signin' | 'signup'>();
+
+  selectTab(tab: 'signin' | 'signup') {
+    this.onTabChange.emit(tab);
+  }
 }
