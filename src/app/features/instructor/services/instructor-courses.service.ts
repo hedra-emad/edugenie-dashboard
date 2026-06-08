3e8a9ed93138b1911a7072713c0e564f1661b3f1
Hedra Emad <emadhedra4@gmail.com>
2026-06-08 21:48:53 +0300

    refactor: clean up code formatting and restore API URL for production

--- src/app/features/instructor/services/instructor-courses.service.ts
+++ src/app/features/instructor/services/instructor-courses.service.ts
@@ -9,18 +9,20 @@ import { InstructorCourse } from '../models/instructor-course.model';
 export class InstructorCoursesService {
   private http = inject(HttpClient);
 
-  // 🔴 Change this to localhost while developing! 
+  // 🔴 Change this to localhost while developing!
   // (Change it back to vercel.app only when you are ready to deploy to production)
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://edugenie-api.vercel.app';
 
   getMyCourses(): Observable<InstructorCourse[]> {
-    return this.http.get<InstructorCourse[]>(`${this.apiUrl}/courses/my-courses`, {
-      withCredentials: true,
-    }).pipe(
-      // 👈 'tap' lets you console.log the data exactly when it arrives from the server!
-      tap((data) => {
-        console.log('✅ Real Fetched Data:', data);
+    return this.http
+      .get<InstructorCourse[]>(`${this.apiUrl}/courses/my-courses`, {
+        withCredentials: true,
       })
-    );
+      .pipe(
+        // 👈 'tap' lets you console.log the data exactly when it arrives from the server!
+        tap((data) => {
+          console.log('✅ Real Fetched Data:', data);
+        }),
+      );
   }
 }
