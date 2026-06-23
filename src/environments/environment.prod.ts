25b8d3986a97ac183384b97acf7e622029977f76
Hedra Emad <emadhedra4@gmail.com>
2026-06-23 22:37:41 +0300

    chore: remove environment files

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://edugenie-api.vercel.app',
-  lessonUploadPreset: 'lessons',
-  thumbnailUploadPreset: 'course_thumbnails',
-  studentAppUrl: 'https://edugenie-student-web.vercel.app'
-};
