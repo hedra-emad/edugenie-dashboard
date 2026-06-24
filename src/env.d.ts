7f2ab09cc13f4e881eea8a3f156224b000b9aa2a
Hedra Emad <emadhedra4@gmail.com>
2026-06-24 06:34:23 +0300

    edit environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 interface ImportMetaEnv {
+  readonly NG_APP_PRODUCTION: string;
   readonly NG_APP_API_URL: string;
   readonly NG_APP_LESSON_UPLOAD_PRESET: string;
   readonly NG_APP_THUMBNAIL_UPLOAD_PRESET: string;
