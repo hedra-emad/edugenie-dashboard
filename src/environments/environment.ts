e6ac88596aeb80b2e8c68f0fd64d576698a7ceb2
Hedra Emad <emadhedra4@gmail.com>
2026-06-24 21:13:41 +0300

    edit envitonment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: 'http://localhost:5000/api',
   lessonUploadPreset: 'lessons',
   thumbnailUploadPreset: 'course_thumbnails',
   studentAppUrl: 'http://localhost:3000',
